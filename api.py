b9eb3fa25356d45218dce7225ee7b0338ee0f4db
vxshnu <vishnunarayanan8690@gmail.com>
2024-11-20 12:52:00 +0530

    deploy

--- api.py
+++ api.py
@@ -6,10 +6,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#print(f"Using device: {device}")
 MODEL_PATH = "vxshnu8690/text-to-pandas"
-model = T5ForConditionalGeneration.from_pretrained(MODEL_PATH).to(device)
+model = T5ForConditionalGeneration.from_pretrained(MODEL_PATH)
 tokenizer = T5Tokenizer.from_pretrained(MODEL_PATH)
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,7 +17,7 @@ def predict():
         input_data = request.json.get("query")
         if not input_data:
             return jsonify({"error": "No query provided"}), 400
-        inputs = tokenizer.encode("generate pandas code: " + input_data, return_tensors="pt").to(device)
+        inputs = tokenizer.encode("generate pandas code: " + input_data, return_tensors="pt")
         outputs = model.generate(inputs, max_length=100, num_beams=4, early_stopping=True)
         decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
         print(decoded_output)
